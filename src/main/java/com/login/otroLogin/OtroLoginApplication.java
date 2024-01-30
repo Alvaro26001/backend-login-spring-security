90ab4e9bc1576d3a4c85ea207c56ce06ed2955c8
Alvaro Rada <alvarorada26@gmail.com>
2024-01-30 11:54:28 -0500

    creacion de  paquestes necesarios

---
+++ src/main/java/com/login/otroLogin/OtroLoginApplication.java
@@ -0,0 +1,13 @@
+package com.login.otroLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtroLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtroLoginApplication.class, args);
+	}
+
+}
