257134d8f690fb0e6500448994e998c91b5154a8
Alvaro Rada <alvarorada26@gmail.com>
2024-01-30 13:54:23 -0500

    se configuro con la implementacion del authenticationManager

--- src/main/java/com/login/otroLogin/models/User.java
+++ src/main/java/com/login/otroLogin/models/User.java
@@ -1,7 +1,10 @@
 package com.login.otroLogin.models;
 
+import com.login.otroLogin.util.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,6 +15,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "users")
@@ -20,7 +30,7 @@ import lombok.experimental.FieldDefaults;
 @NoArgsConstructor
 @Getter
 @Setter
-public class user {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,4 +45,41 @@ public class user {
     @Column
     String password;
 
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority> authorities =
+                this.role
+                        .getPermissions()
+                        .stream()
+                        .map(permissionEnum -> new SimpleGrantedAuthority(permissionEnum.name()))
+                        .collect(Collectors.toList());
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+this.role.name()));
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/login/otroLogin/repository/IUserRepository.java
+++ src/main/java/com/login/otroLogin/repository/IUserRepository.java
@@ -1,7 +1,10 @@
 package com.login.otroLogin.repository;
 
-import org.apache.catalina.User;
+import com.login.otroLogin.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUserRepository extends JpaRepository <User, Long>{
+    Optional<User> findByUsername(String username);
 }
